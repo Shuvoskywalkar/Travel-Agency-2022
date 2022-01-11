0a4a4596b3b3959519775b47c5923ccee25c3ba4
Shuvoskywalkar <shuvokoiri89@gmail.com>
2022-01-11 17:09:40 +0600

    first commit

---
+++ src/Components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC9TqSfhxIpcPLhQNMkECFb4yvKV5rPvaQ",
+  authDomain: "sktravels-2022.firebaseapp.com",
+  projectId: "sktravels-2022",
+  storageBucket: "sktravels-2022.appspot.com",
+  messagingSenderId: "454323355784",
+  appId: "1:454323355784:web:3636429b4a5d94093adf3d",
+  measurementId: "G-KY3NZC9WWL"
+};
+
+  export default firebaseConfig;
